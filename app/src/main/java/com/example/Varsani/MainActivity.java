4a4aa2c5effdda0a26d9b2c009a6ca0e57eecbc6
nival <nivahmichael@gmail.com>
2026-01-08 19:56:30 +0300

    updated mainactivity

--- app/src/main/java/com/example/Varsani/MainActivity.java
+++ app/src/main/java/com/example/Varsani/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
     AdapterProducts adapterProducts;
     HomeFragment homeFragment;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
