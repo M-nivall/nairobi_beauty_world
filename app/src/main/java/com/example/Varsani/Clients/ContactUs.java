f04d3be480dc3860c3d9be17877d52d6b661f4a7
nival <nivahmichael@gmail.com>
2026-01-09 23:24:34 +0300

    implemnted contact us

--- app/src/main/java/com/example/Varsani/Clients/ContactUs.java
+++ app/src/main/java/com/example/Varsani/Clients/ContactUs.java
@@ -12,6 +12,5 @@ public class ContactUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
-
     }
 }
