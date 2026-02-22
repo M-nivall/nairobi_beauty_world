08cd0c0cc221987352edb70e1bed40162faa2577
nival <nivahmichael@gmail.com>
2026-02-22 22:00:42 +0300

    implemented adapter cart

--- app/src/main/java/com/example/Varsani/Clients/Adapters/AdapterCart.java
+++ app/src/main/java/com/example/Varsani/Clients/Adapters/AdapterCart.java
@@ -381,5 +381,4 @@ public class AdapterCart extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     public interface OnMoreButtonClickListener {
         void onItemClick(View view, ProductModal obj, MenuItem item);
     }
-
 }
