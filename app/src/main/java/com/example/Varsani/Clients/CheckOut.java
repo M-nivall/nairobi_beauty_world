a5f93ab5eee63b6b56f092d920117f757f793392
nival <nivahmichael@gmail.com>
2026-02-24 22:32:59 +0300

    updated checkout

--- app/src/main/java/com/example/Varsani/Clients/CheckOut.java
+++ app/src/main/java/com/example/Varsani/Clients/CheckOut.java
@@ -571,4 +571,5 @@ public class CheckOut extends AppCompatActivity {
         RequestQueue requestQueue=Volley.newRequestQueue(getApplicationContext());
         requestQueue.add(stringRequest);
     }
+
 }
